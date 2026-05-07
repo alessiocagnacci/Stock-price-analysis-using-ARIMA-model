00e83523b6f53d62466079efe1afce306938268c
alessiocagnacci <alessio.cagnacci99@gmail.com>
2026-05-07 15:54:36 +0200

    AAPLdaily.R

--- AAPLdaily.R
+++ AAPLdaily.R
@@ -7,46 +7,42 @@ library(ggplot2)
 getSymbols("AAPL", src = "yahoo", from = "2020-01-01", to = Sys.Date())
 chartSeries(AAPL) 
 
-prezzi_adj <- Ad(AAPL)
-plot(prezzi_adj)
+price_adj <- Ad(AAPL)
+plot(price_adj)
 
-rend_log <- diff(log(prezzi_adj))[-1]
-plot(rend_log, main = "Rendimenti logaritmici AAPL")
+ret_log <- diff(log(price_adj))[-1]
+plot(ret_log, main = "Log return AAPL")
 
-adf.test(na.omit(rend_log))
+adf.test(na.omit(ret_log))
 
-log_prezzi <- log(prezzi_adj)
-mod_arima <- auto.arima(log_prezzi)
+log_price <- log(price_adj)
+mod_arima <- auto.arima(log_price)
 summary(mod_arima)
 
 fc <- forecast(mod_arima, h = 30)
 plot(fc)
 
-prezzi_forecast <- exp(fc$mean)
-plot(prezzi_forecast, main = "Previsione AAPL (scala originale)")
+price_forecast <- exp(fc$mean)
+plot(price_forecast, main = "Forecast AAPL")
 
 checkresiduals(mod_arima)
 
-#per grafico
+#plot
 ultima_data <- index(prezzi_adj)[NROW(prezzi_adj)]
-date_forecast <- seq(from = ultima_data + 1, by = "days", length.out = 30)
+dates_forecast <- seq(from = ultima_data + 1, by = "days", length.out = 30)
+dates_forecast <- dates_forecast[weekdays(dates_forecast) %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")]
+dates_forecast <- dates_forecast[1:length(fc$mean)]
 
-# Rimuove sabato e domenica (Yahoo fornisce dati borsistici, no weekend)
-date_forecast <- date_forecast[weekdays(date_forecast) %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")]
-
-# Adatta a lunghezza forecast
-date_forecast <- date_forecast[1:length(fc$mean)]
-
-# Costruisci il dataframe per il plot
+# dataframe for plot
 df_fc <- data.frame(
-  Data = date_forecast,
-  Previsto = as.numeric(exp(fc$mean)),
-  Lower = as.numeric(exp(fc$lower[,2])),  # intervallo 95%
+  Dates = dates_forecast,
+  Forecast = as.numeric(exp(fc$mean)),
+  Lower = as.numeric(exp(fc$lower[,2])),  # CI 95%
   Upper = as.numeric(exp(fc$upper[,2]))
 )
 
 # Grafico
-ggplot(df_fc, aes(x = Data, y = Previsto)) +
+ggplot(df_fc, aes(x = Dates, y = Forecast)) +
   geom_line(color = "blue") +
   geom_ribbon(aes(ymin = Lower, ymax = Upper), fill = "lightblue", alpha = 0.4) +
-  labs(title = "Previsione AAPL (scala originale)", x = "Data", y = "Prezzo previsto")
+  labs(title = "Forecast AAPL ", x = "Date", y = "Predicted price")
